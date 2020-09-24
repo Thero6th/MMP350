a3361047f02069011ce8c251f3d71ca2bf1e303b
Thero6th <48805682+Thero6th@users.noreply.github.com>
2020-09-24 00:00:17 -0400

    Add files via upload

--- assignment2/authentication/js/create.js
+++ assignment2/authentication/js/create.js
@@ -3,8 +3,6 @@ const signupUsername = document.getElementById('sign-up-username');
 const signupEmail = document.getElementById('sign-up-email');
 const signupPassword = document.getElementById('sign-up-password');
 
-// this function allows the user to sign up when they click the button
-signupButton.onclick = function(){
-	// take the email and password and send it to firebase
-	const email = signupEmail.value;
-	const password = signupPassword.value;
+signupButton.onclick = function () {
+        const email = signupEmail.value;
+        const password = signupPassword.value;
