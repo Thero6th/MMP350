d427e8c7b6625378e57e5c3e02e0ab3dafb3ef50
Thero6th <48805682+Thero6th@users.noreply.github.com>
2020-10-02 11:05:09 -0400

    Add files via upload

--- assignment2/authentication/js/create.js
+++ assignment2/authentication/js/create.js
@@ -4,5 +4,23 @@ const signupEmail = document.getElementById('sign-up-email');
 const signupPassword = document.getElementById('sign-up-password');
 
 signupButton.onclick = function () {
-        const email = signupEmail.value;
-        const password = signupPassword.value;
+    const email = signupEmail.value;
+    const password = signupPassword.value;
+    // firebase.auth().createUserWithEmailAndPassword(email, password);
+
+    const promise = firebase.auth().createUserWithEmailAndPassword(email, password);
+    promise.then(updateUser)
+
+    function updateUser(credentials) {
+        const userinfo = {
+            displayName: signUsername.value
+        };
+        credentials.user.updateProfile(userInfo);
+        alert("successfully created a user!")
+    }
+
+    promise.catch(function (error) {
+        console.log(error);
+        alert(error.message);
+    });
+};
